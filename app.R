ab73e86d06c5a1e72dc28ffe285c19399d48c424
Pieter Provoost <pieterprovoost@gmail.com>
2021-02-09 14:29:55 +0100

    integrated priority species lists

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(htmltools)
 library(htmlwidgets)
 library(shinyjs)
+library(stringi)
 
 registerPlugin <- function(map, plugin) {
   map$dependencies <- c(map$dependencies, list(plugin))
@@ -19,7 +20,7 @@ ui <- fluidPage(
     style = "margin-top: 0px;",
     column(12,
       h2("South Pacific invasives dashboard"),
-      p("This dashboard displays a list of WRiMS species observed in the South Pacific based on OBIS data. Species that have been observed in Fiji are flagged as such. Click the row to display all occurrence of a species on the map, or click the scientific name to open the WoRMS page.")
+      p(HTML("This dashboard displays a list of WRiMS species observed in the South Pacific based on OBIS data. Species that have been observed in Fiji are flagged as such. Click the row to display all occurrence of a species on the map, or click the scientific name to open the WoRMS page. The references and remarks columns are taken from the priority species lists at <a href=\"https://github.com/iobis/pacman/tree/main/SpeciesLists\" target=\"_blank\">https://github.com/iobis/pacman/tree/main/SpeciesLists</a>."))
     )
   ),
   fluidRow(
@@ -36,9 +37,16 @@ server <- function(input, output, session) {
   
   cl_fiji <- checklist(wrims = TRUE, areaid = 68) %>%
     select(scientificName, taxonID, class, order, records)
-  
+
+  cl_expert <- read.csv("https://raw.githubusercontent.com/iobis/pacman/main/SpeciesLists/SpeciesList.tsv?token=AADXUOMJQ24AIINJURWKUYDAFLISW", sep = "\t") %>%
+    select(taxonID = AphiaID_accepted, references, remarks = taxonRemarks) %>%
+    mutate(taxonID = as.numeric(taxonID)) %>%
+    group_by(taxonID) %>%
+    summarize(references = paste0(stri_remove_empty_na(references), collapse = "; "), remarks = paste0(stri_remove_empty_na(remarks), collapse = "; "))
+    
   cl <- checklist(wrims = TRUE, geometry = "POLYGON ((-215 -62, -215 14, -86 14, -67 -23, -74 -62, -215 -62))") %>%
     select(scientificName, taxonID, class, order, records) %>%
+    left_join(cl_expert, by = c("taxonID")) %>%
     mutate(fiji = taxonID %in% cl_fiji$taxonID) %>%
     mutate(scientificName = paste0("<a href=\"http://www.marinespecies.org/aphia.php?p=taxdetails&id=", taxonID, "#distributions\" target=\"_blank\">", scientificName, "</a>"))
 
